1aded7bd84f13b3d6736b1eb57626aa4da0c241f
CC8IV <crownedx.creativ@gmail.com>
2023-06-07 14:54:35 +0100

    first commit

---
+++ meanstack/demo/modules/model.js
@@ -0,0 +1,12 @@
+// console.log(__dirname)
+// console.log(__filename)
+
+let names = ["Jill","Sofi", "Lily"]
+let scores = [11, 25, 23]
+
+module.exports.names = names
+module.exports.scores = scores
+exports.data = [1,2,3]
+
+// module.exports.data = "Apples"
+// console.log(module)
